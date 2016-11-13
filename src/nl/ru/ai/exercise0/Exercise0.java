8595cc97aedf06febc2d5fc7d42c5036415f0ddd
Unknown <denisevbaalen@gmail.com>
2016-11-13 12:34:25 +0100

    Initial files

---
+++ src/nl/ru/ai/exercise0/Exercise0.java
@@ -0,0 +1,54 @@
+package nl.ru.ai.exercise0;
+
+public class Exercise0
+{
+  /**
+   * Program main entry point
+   * @param args arguments to the program (ignored)
+   */
+  public static void main(String [] args)
+  {
+    // your code here
+  }
+  /**
+   * Calculate the sum of the specified length arguments
+   * @param a
+   * @param b
+   * @return sum
+   */
+  static Length add(Length a, Length b)
+  {
+    // your code here
+    return null;
+  }
+  /**
+   * Converts the given string in the format m..m:ss to a Length object
+   * @param string
+   * @return corresponding length object
+   */
+  static Length fromString(String string)
+  {
+    // your code here
+    return null;
+  }
+  /**
+   * Converts a given length object into a string in the format m..m:ss
+   * @param length
+   * @return string representation
+   */
+  static String toString(Length length)
+  {
+    // your code here
+    return null;
+  }
+  /**
+   * Reads the cd database from the file 'Nummers.txt' into the specified track array
+   * @param database
+   * @return number of tracks read
+   */
+  static int readDatabase(Track[] database)
+  {
+    // your code here
+    return 0;
+  }
+}
